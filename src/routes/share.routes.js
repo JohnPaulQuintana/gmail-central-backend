f1a9a69d49aec45a867ee4a54b42420eae740544
ExousiaNavi <jpquintana2024@gmail.com>
2026-05-06 21:20:03 +0800

    Update share.routes.js

--- src/routes/share.routes.js
+++ src/routes/share.routes.js
@@ -2,6 +2,6 @@ const router = require("express").Router();
 const shareController = require("../controllers/share.controller");
 
 router.post("/captured", shareController.shared);
-router.post("/transactions", shareController.getTransactions);
+router.get("/transactions", shareController.getTransactions);
 
 module.exports = router;
