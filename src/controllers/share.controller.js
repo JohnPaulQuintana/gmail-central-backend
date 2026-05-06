d7f3248739cf83e0413e55251f030c97d119f569
ExousiaNavi <jpquintana2024@gmail.com>
2026-05-06 20:10:07 +0800

    Update share.controller.js

--- src/controllers/share.controller.js
+++ src/controllers/share.controller.js
@@ -16,6 +16,7 @@ exports.shared = async (req, res) => {
 
     const parsed = parseReceipt(text.replace(/Menu/gi, "").trim());
     console.log("================PARSED==============================")
+    console.log(text.replace(/Menu/gi, "").trim())
     console.log(parsed)
     return res.status(200).json({
       success: true,
